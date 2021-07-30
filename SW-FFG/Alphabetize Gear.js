a3e59f2dd52fbcead98d0350253dd92c1e4d5ca2
halfwalk <halfwalkmedia@gmail.com>
2021-07-30 15:27:47 -0400

    Create Alphabetize Gear.js

---
+++ SW-FFG/Alphabetize Gear.js
@@ -0,0 +1,34 @@
+// This macro only works with one token selected
+// It alphabetizes all the items in the "gear" category in their inventory
+
+let tokens = canvas.tokens.controlled;
+let gearToSort = [];
+
+if (tokens.length != 1) ui.notifications.error("Select a single token before running this macro");
+
+else {
+    let tActor = token.actor;
+    let gearList = tActor.items;
+	let gearListSpec = gearList.filter(i => i.data.type === "gear");
+
+// check if item is "gear" type. if so, add it to array and delete it from actor
+	for (let element of gearList) {
+		if (element.data.type === "gear") {
+			 gearToSort.push(element);
+			await element.delete() }
+	}
+	
+// sort the items in the array alphabetically by item name
+	gearToSort.sort(function(a,b) {
+		if (a.name < b.name) { return -1; }
+		if (a.name > b.name) { return 1; }
+		return 0;
+	});
+	
+	
+// now put the sorted array back on the actor
+	tActor.update({
+		"items": gearToSort
+	});
+
+}
