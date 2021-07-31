09559373417fadc4adb767bfabda59a422536d5f
halfwalk <halfwalkmedia@gmail.com>
2021-07-31 14:14:40 -0400

    alphabetize gear macro
    added dialog and options

--- SW-FFG/Alphabetize Gear.js
+++ SW-FFG/Alphabetize Gear.js
@@ -1,29 +1,94 @@
-// This macro only works with one token selected
-// It alphabetizes all the items in the "gear" category in their inventory
+const characters = game.actors.filter(a => a.data.type === "character" && a.hasPlayerOwner);
+let charList = "", toDo = ""
+const itemTypes = ["Weapon","Armour","Talent","Gear"];
 
-let tokens = canvas.tokens.controlled;
-let gearToSort = [];
 
-if (tokens.length != 1) ui.notifications.error("Select a single token before running this macro");
+for (let item of itemTypes) {
+	toDo += `
+		
+		<input type="checkbox" name="${item}" id="${item}" value="${item}">
+		<label for="${item}">${item}</label>
+		`
+}
 
-else {
-    let tActor = token.actor;
-    let gearList = tActor.items;
+populateList(characters, charList,3);
 
-	for (let element of gearList.filter(i => i.data.type === "gear")) {
+let dialogEditor = new Dialog({
+	title: `Sort Items Alphabetically`,
+	content: `
+	<div class="form-group">
+		<label>Character</label>
+		<select id = "charName" name="charName">
+		${charList}
+		</select>
+	</div>
+	<hr>
+	<br>
+	<div class="form-group">
+	<label>Things to sort</label>
+		${toDo}
+	</div>
+	<br>
+	`,
+	buttons: {
+		okay: {
+			label: "Okay",
+			callback: (html) => doTheThing(html)
+		},
+		cancel: {
+			label: "Cancel"
+		}
+	},
+}).render(true);
+
+async function doTheThing(html) {
+	let itemsToDo = []
+	const element = html.find('[name="charName"]')[0].value;
+	
+	// see which types are checked
+	for (let i of itemTypes) {
+		if (html.find(`[id="${i}"]`)[0].checked) itemsToDo.push(i.toLowerCase());
+	}
+	console.log(itemsToDo);
+	console.log(element);
+	
+	
+	// find the desired actor and perform the sorting
+	
+	for ( let i of game.actors ) {
+		if (i.id === element) {
+			for (let j of itemsToDo) {
+				await sortItems (i,j);
+			}
+		}
+	}
+	
+}
+
+function populateList(x,y) {
+	y = ""
+	x.forEach(a => {
+		charList += `<option value=${a.id}>${a.data.name}</option>`
+	});
+}
+	
+async function sortItems (actor,itemType) {
+
+    let gearList = actor.items;
+	let gearToSort = [];
+
+	for (let element of gearList.filter(i => i.data.type === itemType)) {
 		gearToSort.push(element);
 		await element.delete();
 	}
 	
-// sort the items in the array alphabetically by item name
 	gearToSort.sort(function(a,b) {
 		if (a.name < b.name) { return -1; }
 		if (a.name > b.name) { return 1; }
 		return 0;
 	});
 	
-// now put the sorted array back on the actor
-	tActor.update({
+	actor.update({
 		"items": gearToSort
 	});
 }
