03b723816eaa86c0df0022ec6c0ac5b00603869c
halfwalk <halfwalkmedia@gmail.com>
2021-07-31 15:50:09 -0400

    Update Alphabetize Gear.js
    fixed some nonsense and commented everything

--- SW-FFG/Alphabetize Gear.js
+++ SW-FFG/Alphabetize Gear.js
@@ -1,20 +1,27 @@
 const characters = game.actors.filter(a => a.hasPlayerOwner);
-let charList = "", toDo = ""
+let toDo = "", charList = ""
 const itemTypes = ["Weapon","Armour","Talent","Gear"];
 
 
+// build html for checkboxes from itemTypes array
+
 for (let item of itemTypes) {
 	let plural = ""
+
+	// pluralize labels for "weapons" and "talents"
+
 	if (item.at(item.length-1) != "r") plural = "s"
-	toDo += `
-		
+	toDo += `	
 		<input type="checkbox" name="${item}" id="${item}" value="${item}">
 		<label for="${item}">${item}${plural}</label>
-		<br>
-		`
+		<br>`
 }
 
-populateList(characters, charList,3);
+// build html for dropdown from characters array
+
+charList = populateList(characters, charList);
+
+//create dialog box
 
 let dialogEditor = new Dialog({
 	title: `Sort Items Alphabetically`,
@@ -45,11 +52,14 @@ let dialogEditor = new Dialog({
 	default: "okay"
 }).render(true);
 
+// okay button callback function
+
 async function doTheThing(html) {
 	let itemsToDo = []
 	const element = html.find('[name="charName"]')[0].value;
 	
-	// see which types are checked
+	// see which types are checked. populate array with item types
+	
 	for (let i of itemTypes) {
 		if (html.find(`[id="${i}"]`)[0].checked) itemsToDo.push(i.toLowerCase());
 	}
@@ -66,12 +76,18 @@ async function doTheThing(html) {
 	
 }
 
+// fill the string y with html based on the names of the actors in array x
+// probably doesn't need to be its own function... just did it that way so I can reuse in another macro
+
 function populateList(x,y) {
 	y = ""
 	x.forEach(a => {
-		charList += `<option value=${a.id}>${a.data.name}</option>`
+		y += `<option value=${a.id}>${a.data.name}</option>`
 	});
+	return y;
 }
+
+// alphabetically sort selected item type on actor sheet
 	
 async function sortItems (actor,itemType) {
 
