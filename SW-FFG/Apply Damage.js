14103cccdfd8cc7cb641e1385b30123a44fdbe82
halfwalk <halfwalkmedia@gmail.com>
2021-09-03 22:40:29 -0400

    Update Apply Damage.js
    added options to do strain damage and to ignore soak.
    
    fixed bug from empty damage

--- SW-FFG/Apply Damage.js
+++ SW-FFG/Apply Damage.js
@@ -5,6 +5,10 @@ let dialogContent = `
 		<input type="text" id="damage" name="damage"><br>
 		<label for="pierce">Pierce</label>
 		<input type="text" id="pierce" name="pierce" value="0">
+		<input type="checkbox" id="ignore" name="ignore">
+		<label for="ignore">Ignore Soak</label><br>
+		<input type="checkbox" id="strain" name="strain">
+		<label for="strain">Strain</label>
 	</div>
 	`;
 
@@ -24,13 +28,23 @@ let dialog = new Dialog({
 async function applyDamage(html) {
 	let damage = parseInt(html.find('[name="damage"]')[0].value);
 	let pierce = parseInt(html.find('[name="pierce"]')[0].value);
+	let isStrain = html.find('[name="strain"]')[0].checked;
+	let ignoreSoak = html.find('[name="ignore"]')[0].checked;
+	console.log(ignoreSoak);
+	
+	if (!damage) damage = 0;
+	
+	let target = "wounds";
+	if (isStrain) target = "strain";
+	console.log(token.actor.data.data.stats[target].value);
+	
 	let soak = token.actor.data.data.stats.soak.value;
+	let addl = (ignoreSoak) ? 0 : Math.max((soak - pierce),0);
 	
-	let newWounds = token.actor.data.data.stats.wounds.value + (damage - Math.max((soak - pierce),0));
+	let newWounds = token.actor.data.data.stats[target].value + (damage - addl);
 	
-	await token.actor.update({"data.stats.wounds.value":newWounds
-		
-	});
+	if (isStrain) await token.actor.update({"data.stats.strain.value":newWounds});
+	else await token.actor.update({"data.stats.wounds.value":newWounds});
 					
 }
 }
